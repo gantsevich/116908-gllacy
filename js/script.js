09f439c2c0ba33aa9d08e6842fd73a438aacb098
Shururup <vickt.uwarowa@yandex.ru>
2017-05-09 13:17:50 +0500

    Задание 9
    К стилям добавлены префиксы, скрипты собраны в один файл, подключены
    минифицированные стили и скрипт. Проведено прогрессивное улучшение -
    кнопка формы ведет на новую страницу, для карты задано фоновое
    изображение на случай отсутствия js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+ymaps.ready(function () {
+		var myMap = new ymaps.Map('map', {
+				center:[59.939180, 30.327346],
+				zoom:16
+		}, {
+				searchControlProvider: 'yandex#search'
+		});
+		var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+				iconLayout: 'default#image',
+				iconImageHref: 'img/map_pointer.png',
+				iconImageSize: [218, 142],
+				iconImageOffset: [-240, -85]
+		});
+
+		myMap.geoObjects
+								.add(myPlacemark);
+});
+
 var link = document.querySelector(".feedback_btn");
 
 var popup = document.querySelector(".feedback");
